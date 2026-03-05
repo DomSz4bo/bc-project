10cafcd739df0663633fa3f4a4afb1f737c85b7b
Dominik Szabó <szabo175@uniba.sk>
2026-03-05 23:17:24 +0100

    Add supervisor output model

--- src/agents/supervisor.py
+++ src/agents/supervisor.py
@@ -1,5 +1,34 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
 from src.graph.state import AgentState
-from src.utils.llm import llm
+from src.utils.llm import gemini_flash
+
+SYSTEM_PROMPT = ""
+
+
+class SupervisorOutput(BaseModel):
+    next_step: Literal["DESIGN", "IMPLEMENT", "USER"] = Field(
+        description=("The next step. Must be one of DESIGN, IMPLEMENT and USER. "),
+    )
+    refined_intent: str | None = Field(
+        default=None,
+        description=(
+            "If next step is DESIGN, the summarized goal after any clarifications. "
+            "Be detailed, include anything that may help the design team adhere to "
+            "the user's requirements. "
+            "Else onit this field."
+        ),
+    )
+    message_to_user: str | None = Field(
+        description=(
+            "If next step is USER, a message aimed at the user. Else omit this field."
+        ),
+    )
+
+
+llm_with_structure = gemini_flash.with_structured_output(SupervisorOutput)
 
 async def supervisor(state: AgentState) -> AgentState:
     return state
