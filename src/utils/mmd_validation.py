fd9a5f415ed5b00341efa668182a7f4639b4ad10
Dominik Szabó <szabo175@uniba.sk>
2026-03-12 23:07:47 +0100

    Add module main test to `mmd_validation`

--- src/utils/mmd_validation.py
+++ src/utils/mmd_validation.py
@@ -1,3 +1,5 @@
+import asyncio
+from time import perf_counter
 from typing import NamedTuple
 
 from langchain_mcp_adapters.client import MultiServerMCPClient
@@ -14,6 +16,8 @@ client = MultiServerMCPClient(
     }
 )
 
+rendering_tool = asyncio.run(client.get_tools())[0]
+
 
 class ValidationResult(NamedTuple):
     is_valid: bool
@@ -21,10 +25,8 @@ class ValidationResult(NamedTuple):
 
 
 async def validate_mermaid(mermaid_code: str) -> ValidationResult:
-    rendering_tool = (await client.get_tools())[0]
-
     try:
-        await rendering_tool.ainvoke({"mermaid": mermaid_code})
+        await rendering_tool.ainvoke({"mermaid": mermaid_code, "outputType": "mermaid"})
         is_valid = True
     except McpError as e:
         is_valid = False
@@ -32,3 +34,15 @@ async def validate_mermaid(mermaid_code: str) -> ValidationResult:
         error_msg = error_msg[error_msg.find(": ") + 2 :]
 
     return ValidationResult(is_valid, None if is_valid else error_msg)
+
+
+if __name__ == "__main__":
+    mmd_code = """sequenceDiagram
+    Alice->>+John: Hello John, how are you?
+    Alice->>+John: John, can you hear me?
+    John-->>-Alice: Hi Alice, I can hear you!
+    John-->>-Alice: I feel great!"""
+    start = perf_counter()
+    result = asyncio.run(validate_mermaid(mmd_code))
+    end = perf_counter()
+    print(f"Result: {result}, \t time: {end - start} s")
