2251e294f7dd832a865fcbc08e38d988e3b049e6
Dominik Szabó <szabo175@uniba.sk>
2026-04-13 16:51:25 +0200

    Add gemma4

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -76,3 +76,4 @@ gemini_3p1_flash_lite = init_chat_model(
     "gemini-3.1-flash-lite-preview", model_provider="google_genai"
 )
 gemma_3_27b = init_chat_model("gemma-3-27b-it", model_provider="google_genai")
+gemma_4_31b = init_chat_model("gemma-4-31b-it", model_provider="google_genai")
